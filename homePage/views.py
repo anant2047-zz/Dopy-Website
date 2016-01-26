3625e1df965533a6f1461b3b9722dfc5500b5997
Snimarpal Singh <Snimarpal Singh>
2016-01-26 16:39:21 +0530

    Added Dynamic Tag generation using django and improved admin

--- homePage/views.py
+++ homePage/views.py
@@ -6,40 +6,79 @@ from django.http import HttpResponse, Http404
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.core.mail import send_mail
-
+import os,random
 
 # Create your views here.
 from django.shortcuts import render
-from .forms import ContactForm, SignUpForm
+from .forms import ContactForm
 
 # Create your views here.
 def home(request):
-	# if request.user.is_authenticated():
-	# 	title = title + ", %s" %(request.user)
-	#add a form
-	# if request.method == "POST":
-	# 	print(request.POST)
-	form = SignUpForm(request.POST or None)#makes sure method is POST
+	path = os.path.join(os.path.dirname(os.path.join(settings.BASE_DIR)), "static_in_env","media_root" ,"events")
+	recent_dir = [(os.path.getctime(os.path.join(path,f)), f)for f in os.listdir(path)]
+	recent_dir.sort(reverse = True)
+	event_title = os.listdir(os.path.join(os.path.dirname(os.path.join(settings.BASE_DIR)), "static_in_env","media_root" ,"events"))
+	random_display_image = []
+	for i in range(3):
+		random_display_image.append( random.choice(os.listdir(os.path.join(path,recent_dir[i][1],"sliderImages","img")) ))
+	
+
+	random_panel_image = []
+	for i,p in recent_dir:
+		k=0
+		random_panel_image.append( random.choice(os.listdir(os.path.join(path,recent_dir[k][1],"panelImages","img")) ))
+		k+=1
+
+	# print(random_panel_image)
+	# print(event_title)
+
+	slider_name = []
+	slider_title = []
+	thumbnail_name = []
+	thumbnail_title = []
+
+	location = os.path.join(os.path.dirname(os.path.join(settings.BASE_DIR)), "static_in_env","media_root")
+
+	i = 1
+	for f in os.listdir(os.path.join(location,"home_slider","img")):
+	#slider image can have atmost 12 images		
+		if not i > 12:
+			slider_name.append(f)
+			slider_title.append(str(i))
+		else :
+			break 
+		i+=1
+	print(slider_name)
+
+	i=1
+	for f in os.listdir(os.path.join(location,"home_slider","thumbnails")):
+		#thumbnails can have atmost 12 images		
+		if not i > 12:
+			thumbnail_name.append(f)
+			thumbnail_title.append(str(i))
+		else :
+			break 
+		i+=1
+
 
 	context = {
-	"form":form,
+		"event1" : recent_dir[0][1],
+		"event1_image" : random_display_image[0],
+		"event2" : recent_dir[1][1],
+		"event2_image" : random_display_image[1],
+		"event3" : recent_dir[2][1],
+		"event3_image" : random_display_image[2],
+		#slider
+		"thumbnails_list" : zip(thumbnail_name,thumbnail_title),
+		"slider_list":zip(slider_name,slider_title),
+		# upper panel
+		"title":event_title,
+		"panel_list":zip(event_title,random_panel_image),
 	}
-	if form.is_valid():
-		#print(request.POST['email']) Will fetch raw data and skip any validation even after putting it under is_valid function.So,its NOT Recommended
-		#form.save() will save directly and will not allow us to use any filters
-		instance = form.save(commit=False) #helps to filter data because its not saved and we can do whatever we want with data till we save it.
-		if not instance.fullname:
-			#fullname="whatever" can be done as well
-			instance.fullname = "Yoman"
-		instance.save()
-		# print(instance)
-		# print(instance.timestamp)
+	
 
-	# 	context = {
-	# "title":"Thank You!",
-	# }
-	return render(request,"index.html",context)
-	# return render(request, "index.html")
+
+	return render(request, "index.html",context)
 
 def about(request):
 	return render(request,"about.html")
@@ -47,40 +86,62 @@ def about(request):
 def contact(request):
 	return render(request,"contact-us.html")
 
-# def test(request):
+def test(request):
 
-# 	some_list = []
-# 	for i in range(100):
-# 		some_list.append(str(i))
+	some_list = []
+	for i in range(100):
+		some_list.append(str(i))
 
-	
-# 	context = {
-# 		"some_list" : some_list,
-# 	}
-# 	return render(request,"test.html",context)
+	context = {
+		"some_list" : some_list,
+	}
+	return render(request,"test.html",context)
 
 
 def gallery(request):
-	# event_name = request.get_full_path().replace("/gallery/","").lower()
-	# slider_list = []
-	# location = os.path.join(os.path.dirname(os.path.join(settings.BASE_DIR)), "static_in_env","media_root" ,"events",event_name)
+	event_name = request.get_full_path().replace("/gallery/","").lower()
+	slider_name = []
+	slider_title = []
+	thumbnail_name = []
+	thumbnail_title = []
+	panel_name = []
 
-	# #slider image and thumbnail can have atmost 10 images
-	# for f in os.listdir(os.path.join(location,"sliderImages","img")):
-	# 	slider_list.append(str(i))
+	location = os.path.join(os.path.dirname(os.path.join(settings.BASE_DIR)), "static_in_env","media_root" ,"events",event_name)
 
-	# #slider image and thumbnail can have atmost 12 images
-	# if not len(slider_list) <= 12:
-	# 	slider_list = []
-	# 	for i in range(1,13):
-	# 		slider_list.append(str(i))
+	i = 1
+	for f in os.listdir(os.path.join(location,"sliderImages","img")):
+	#slider image can have atmost 12 images		
+		if not i > 12:
+			slider_name.append(f)
+			slider_title.append(str(i))
+		else :
+			break 
+		i+=1
 
-	# context = {
-	# 	"title":event_name,
-	# 	"thumbnail_list" : slider_list,
-	# 	"slider_list":slider_list,
-	# }
-	return render(request,"GalleryScroll.html")
+	i=1
+	for f in os.listdir(os.path.join(location,"thumbnails","img")):
+		#thumbnails can have atmost 12 images		
+		if not i > 12:
+			thumbnail_name.append(f)
+			thumbnail_title.append(str(i))
+		else :
+			break 
+		i+=1
+
+	for f in os.listdir(os.path.join(location,"panelImages","img")):
+		#panelImages can have atmost 12 images		
+		panel_name.append(f)
+
+
+
+	context = {
+		"title":event_name,
+		"thumbnails_list" : zip(thumbnail_name,thumbnail_title),
+		"slider_list":zip(slider_name,slider_title),
+		"panel_list":panel_name,
+	}
+
+	return render(request,"GalleryScrollTemplate.html",context)
 
 
 
@@ -109,7 +170,7 @@ def contact(request):
 			form_message, 
 			form_email)
 
-		#send_mail(subject, contact_message, from_email, to_email, fail_silently=True)
+		send_mail(subject, contact_message, from_email, to_email, fail_silently=True)
 		context = {
 
 		}
