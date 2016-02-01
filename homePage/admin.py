df1b207b66436ffe83d283df857dc8c061c412a3
Snimarpal Singh <Snimarpal Singh>
2016-02-01 15:47:36 +0530

    Added css to registrations and sending email via smtp,added captcha to contact-us

--- homePage/admin.py
+++ homePage/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from .forms import SignUpForm
-from .models import SignUp
+# from .forms import SignUpForm
+# from .models import SignUp
 
 # Register your models here.
-class SignUpAdmin(admin.ModelAdmin):
-	list_display = ["email", "timestamp", "updated"]
-	# class Meta:
-	# 	model = SignUp
-	form = SignUpForm
+# class SignUpAdmin(admin.ModelAdmin):
+# 	list_display = ["email", "timestamp", "updated"]
+# 	# class Meta:
+# 	# 	model = SignUp
+# 	form = SignUpForm
 
 
 
-admin.site.register(SignUp, SignUpAdmin)
+# admin.site.register(SignUp, SignUpAdmin)
--- homePage/forms.py
+++ homePage/forms.py
@@ -1,26 +1,38 @@
 from django import forms
-from .models import SignUp
+from captcha.fields import CaptchaField
+# from .models import SignUp
+from captcha.fields import CaptchaField
 
 
 class ContactForm(forms.Form):#use model to save the data in database just as SignUpForm
 	fullname = forms.CharField(required=False)
 	email = forms.EmailField()
 	message = forms.CharField()
+	captcha = CaptchaField()
 
-class SignUpForm(forms.ModelForm):
-	password = forms.CharField(widget=forms.PasswordInput)
-	class Meta:
-		model = SignUp
-		widgets = {'password': forms.PasswordInput(),}
-		fields = ['fullname', 'email','password']
+# class SignUpForm(forms.ModelForm):
+# 	password = forms.CharField(widget=forms.PasswordInput)
+# 	class Meta:
+# 		model = SignUp
+# 		widgets = {'password': forms.PasswordInput(),}
+# 		fields = ['fullname', 'email','password']
 
-	def clean_email(self):
-		print("working!")
-		email = self.cleaned_data.get('email')
-		base_email, provider = email.split("@")
-		domain, extension = provider.split(".")
-		# if not domain == "gmail":
-		# 	raise forms.ValidationError("Please enter a valid gmail email")
-		if not extension == "edu":
-			raise forms.ValidationError("Please enter a valid edu email")
-		return email
+# 	def clean_email(self):
+# 		print("working!")
+# 		email = self.cleaned_data.get('email')
+# 		base_email, provider = email.split("@")
+# 		domain, extension = provider.split(".")
+# 		# if not domain == "gmail":
+# 		# 	raise forms.ValidationError("Please enter a valid gmail email")
+# 		if not extension == "edu":
+# 			raise forms.ValidationError("Please enter a valid edu email")
+# 		return email
+
+	
+
+# class CaptchaTestModelForm(forms.ModelForm):
+# 	captcha = CaptchaField()
+# 	class Meta:
+# 		model = MyModel
+
+	
